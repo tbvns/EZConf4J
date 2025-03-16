02e7d49d58ecf88917a716c43eff0502d3fb7e5f
____tbvns____ <tbvns601@gmail.com>
2025-03-16 19:41:30 +0100

    Should fix an error I had

--- src/main/java/xyz/tbvns/EZConfigUtils.java
+++ src/main/java/xyz/tbvns/EZConfigUtils.java
@@ -7,12 +7,36 @@ import com.google.gson.JsonParser;
 
 import java.io.File;
 import java.lang.reflect.Field;
+import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 public class EZConfigUtils {
-    public static String getJarPath(Class clazz) throws URISyntaxException {
-        return new File(clazz.getProtectionDomain().getCodeSource().getLocation()
-                .toURI()).getPath();
+    public static String getJarPath(Class<?> clazz) {
+        try {
+            String className = clazz.getName().replace('.', '/') + ".class";
+            URL classUrl = clazz.getClassLoader().getResource(className);
+
+            if (classUrl == null) {
+                throw new IllegalStateException("Cannot find class resource");
+            }
+
+            String urlString = classUrl.toString();
+
+            if (urlString.startsWith("jar:file:")) {
+                // Extract actual file path from JAR URL
+                int endIndex = urlString.indexOf("!");
+                return new File(new URI(urlString.substring(4, endIndex))).getAbsolutePath();
+            } else if (urlString.startsWith("file:")) {
+                // Handle class files in directory
+                int endIndex = urlString.length() - className.length() - 2;
+                return new File(new URI(urlString.substring(0, endIndex))).getAbsolutePath();
+            }
+
+            throw new IllegalStateException("Unsupported class location: " + urlString);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to get JAR path", e);
+        }
     }
 
     public static String getJarParent(Class clazz) throws URISyntaxException {
