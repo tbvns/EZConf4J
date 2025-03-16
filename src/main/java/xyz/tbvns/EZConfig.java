2c7a156fb19aafc027798e88da8d02f72cb43046
____tbvns____ <tbvns601@gmail.com>
2025-03-16 21:03:05 +0100

    Revert everything and use a different approach

--- src/main/java/xyz/tbvns/EZConfig.java
+++ src/main/java/xyz/tbvns/EZConfig.java
@@ -8,25 +8,19 @@ import org.apache.commons.io.FileUtils;
 import org.reflections.Reflections;
 import xyz.tbvns.Exeptions.NoEmptyConstructor;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
 public class EZConfig {
-    @Getter @Setter
+    @Getter
     private static String configFolderName = "Config";
     @Getter
     private static final List<Class<? extends Config>> registeredClasses = new ArrayList<>();
@@ -37,6 +31,20 @@ public class EZConfig {
      */
     public static void registerClassPath(String classpath) {
         registeredClasses.addAll(new Reflections(classpath).getSubTypesOf(Config.class));
+    }
+
+    /**
+     * @apiNote The ConfigFolderName is ignored if configFolder is set by hand.
+     */
+    public static void setConfigFolderName(String configFolderName) {
+        EZConfig.configFolderName = configFolderName;
+    }
+
+    /**
+     * @apiNote The ConfigFolderName is ignored if configFolder is set by hand.
+     */
+    public static void setConfigFolder(String path) {
+        EZConfigUtils.configFolder = path;
     }
 
     public static void save() throws Exception {
@@ -70,36 +78,36 @@ public class EZConfig {
     }
 
     public static void createDefault(Class<? extends Config> c) throws Exception {
-        // Get config directory path
-        String configDirPath = EZConfigUtils.getConfigFolder(c);
-        Path configDir = Paths.get(configDirPath);
-
-        // Create config directory with proper permissions
-        if (!Files.exists(configDir)) {
-            Files.createDirectories(configDir);
+        File folder = new File(EZConfigUtils.getConfigFolder(c));
+        if (!folder.exists()) {
+            folder.mkdir();
         }
 
-        // Create config file path
-        Path configFile = Paths.get(EZConfigUtils.getConfig(c.getSimpleName(), c));
+        Gson gson = new GsonBuilder()
+                .serializeNulls()
+                .excludeFieldsWithModifiers()
+                .setPrettyPrinting()
+                .create();
 
-        // Only create file if it doesn't exist
-        if (!Files.exists(configFile)) {
-            // Create empty file with proper permissions
-            Files.createFile(configFile);
+        File file = new File(EZConfigUtils.getConfig(c.getSimpleName(), c));
 
-            // Initialize default config
-            Constructor<?> constructor = c.getDeclaredConstructor();
-            Config defaultConfig = (Config) constructor.newInstance();
-
-            // Write config with proper file locking
-            try (BufferedWriter writer = Files.newBufferedWriter(configFile, StandardCharsets.UTF_8)) {
-                new GsonBuilder()
-                        .setPrettyPrinting()
-                        .create()
-                        .toJson(defaultConfig, writer);
-            }
+        if (!file.exists()) {
+            file.createNewFile();
         }
-    }    public static void load() throws Exception {
+
+        if (c.getDeclaredConstructor().isVarArgs()) {
+            throw new NoEmptyConstructor("No empty constructor found on class " + c.getSimpleName());
+        }
+
+        FileUtils.writeStringToFile(
+                file,
+                gson.toJson(c.getDeclaredConstructor().newInstance()),
+                Charset.defaultCharset()
+        );
+
+    }
+
+    public static void load() throws Exception {
         for (Class<? extends Config> registeredClass : registeredClasses) {
             File file = new File(EZConfigUtils.getConfig(registeredClass.getSimpleName(), registeredClass));
             if (!file.exists()) {
--- src/main/java/xyz/tbvns/EZConfigUtils.java
+++ src/main/java/xyz/tbvns/EZConfigUtils.java
@@ -7,98 +7,31 @@ import com.google.gson.JsonParser;
 
 import java.io.File;
 import java.lang.reflect.Field;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
+import java.net.URISyntaxException;
 
 public class EZConfigUtils {
-    private static final String APP_NAME = "nsi-website";
-    private static final String CONFIG_DIR_NAME = "Config";
+    public static String configFolder;
 
-    public static String getJarPath(Class<?> clazz) {
-        try {
-            String className = clazz.getName().replace('.', '/') + ".class";
-            URL classUrl = clazz.getClassLoader().getResource(className);
-
-            if (classUrl == null) {
-                throw new IllegalStateException("Cannot find class resource");
-            }
-
-            String urlString = classUrl.toString();
-            String decodedPath = "";
-
-            if (urlString.startsWith("jar:nested:")) {
-                // Spring Boot nested JAR handling
-                int endIndex = urlString.indexOf("!/");
-                String path = urlString.substring("jar:nested:".length(), endIndex);
-                decodedPath = decodePath(path);
-            } else if (urlString.startsWith("jar:file:")) {
-                // Regular JAR file
-                int endIndex = urlString.indexOf("!");
-                decodedPath = decodePath(urlString.substring("jar:file:".length(), endIndex));
-            } else if (urlString.startsWith("file:")) {
-                // IDE execution
-                int endIndex = urlString.length() - className.length() - "file:".length() - 2;
-                decodedPath = decodePath(urlString.substring("file:".length(), endIndex));
-            }
-
-            return Paths.get(decodedPath).normalize().toString();
-
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to get JAR path", e);
-        }
+    public static String getJarPath(Class clazz) throws URISyntaxException {
+        return new File(clazz.getProtectionDomain().getCodeSource().getLocation()
+                .toURI()).getPath();
     }
 
-    private static String decodePath(String encodedPath) throws Exception {
-        String decoded = URLDecoder.decode(encodedPath, StandardCharsets.UTF_8.name());
-
-        // Fix Windows paths
-        if (System.getProperty("os.name").toLowerCase().contains("win")) {
-            if (decoded.startsWith("/") && decoded.length() > 2 && decoded.charAt(2) == ':') {
-                return decoded.substring(1);
-            }
-        }
-        return decoded;
+    public static String getJarParent(Class clazz) throws URISyntaxException {
+        return new File(getJarPath(clazz)).getParent();
     }
 
-    public static String getConfigFolder(Class<?> clazz) {
-        if (isRunningFromJar(clazz)) {
-            return getSystemConfigDirectory();
-        }
-        return getDevelopmentConfigDirectory();
+
+    /**
+     * @apiNote The ConfigFolderName is ignored if configFolder is set by hand.
+     */
+    public static String getConfigFolder(Class clazz) throws URISyntaxException {
+        if (configFolder != null) return configFolder;
+        return getJarParent(clazz) + File.separator + EZConfig.getConfigFolderName();
     }
 
-    private static boolean isRunningFromJar(Class<?> clazz) {
-        String className = clazz.getName().replace('.', '/') + ".class";
-        URL resource = clazz.getClassLoader().getResource(className);
-        return resource != null && resource.toString().startsWith("jar:");
-    }
-
-    private static String getSystemConfigDirectory() {
-        String baseDir = System.getProperty("user.home");
-
-        if (System.getProperty("os.name").toLowerCase().contains("linux")) {
-            String xdgConfigHome = System.getenv("XDG_CONFIG_HOME");
-            if (xdgConfigHome != null && !xdgConfigHome.isEmpty()) {
-                baseDir = xdgConfigHome;
-            } else {
-                baseDir += "/.config";
-            }
-        } else if (System.getProperty("os.name").toLowerCase().contains("win")) {
-            baseDir = System.getenv("APPDATA");
-        }
-
-        return Paths.get(baseDir, APP_NAME, CONFIG_DIR_NAME).toString();
-    }
-
-    private static String getDevelopmentConfigDirectory() {
-        return Paths.get("").toAbsolutePath().toString()
-                + File.separator + CONFIG_DIR_NAME;
-    }
-
-    public static String getConfig(String name, Class<?> clazz) {
-        return Paths.get(getConfigFolder(clazz), name + ".json").toString();
+    public static String getConfig(String name, Class clazz) throws URISyntaxException {
+        return getConfigFolder(clazz) + File.separator + name + ".json";
     }
 
     public static <T> T deserializeAndUpdateStaticFields(String jsonContent, Class<T> clazz) throws Exception {
@@ -109,8 +42,8 @@ public class EZConfigUtils {
         for (Field field : clazz.getDeclaredFields()) {
             if (java.lang.reflect.Modifier.isStatic(field.getModifiers())) {
                 field.setAccessible(true);
-                String fieldName = field.getName();
 
+                String fieldName = field.getName();
                 if (jsonObject.has(fieldName)) {
                     JsonElement jsonElement = jsonObject.get(fieldName);
                     Object value = gson.fromJson(jsonElement, field.getType());
@@ -118,6 +51,8 @@ public class EZConfigUtils {
                 }
             }
         }
+
         return object;
     }
+
 }
